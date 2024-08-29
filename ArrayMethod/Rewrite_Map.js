ac076206cd69507dccf17286bba5c8dd44b5d1bb
LAPTOP-GBK77T8O\Asus <levietnguyenhung0711@gmail.com>
2024-08-29 09:48:38 +0700

    done rewrite map method in array

---
+++ ArrayMethod/Rewrite_Map.js
@@ -0,0 +1,17 @@
+var arr=[2,3,4,5];
+
+
+Array.prototype.mapfn= function(callbackfn){
+    var nArr=[];
+    var value;
+    for (var i=0;i<this.length; i++){
+        value=callbackfn(this[i]);
+        nArr.push(value); 
+    }
+    return nArr;
+};
+//Nhan doi cac gia tri so trong mang
+var newArr=arr.mapfn((num)=>num*2);
+
+
+console.log(newArr);
